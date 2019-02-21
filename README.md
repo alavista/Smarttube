5fb70f12f6110709dc726fcbeae0d5ab33ebfffe
Robert Beck <beckr@mail.uni-paderborn.de>
2019-02-21 12:24:31 +0000

    EEPROM Definition, GET API
    In ReadMe and somd Dummy functions
    Added GET API

--- README.md
+++ README.md
@@ -19,3 +19,17 @@ Project for the "Tube challenge" submitted for the ITQ Smart Green Island Makeat
 * http://192.168.4.1/ident: Returns mediaction identification (Dummy)
 * http://192.168.4.1/exp: Returns expiration date (Dummy)
 * http://192.168.4.1/set/value: Sets count to "value" (Integer), returns success or failure
+
+
+# EEPROM structure
+* 16 byte Blocksize
+Starting adress is (Device ID*16)
+| Byte          | Content |
+| ------------- | ------------- |
+| 0             | DeviceID      |
+| 1             | Expiration Year |
+| 2             | Expiration Month|
+| 3             | tmpDropcount |
+| 4,5           | glocalDropcount|
+| 6             | UserID|
+| 7-15          | Free|
--- Smarttube/Smarttube.ino
+++ Smarttube/Smarttube.ino
@@ -4,15 +4,26 @@
 #include <ESPAsyncWebServer.h>
 #include <ESPmDNS.h>
 #include <WiFiUdp.h>
+//#include <Dropcounter.h>
+//#include <Memorymanager.h>
 
 //Wifi 
 //const char *ssid = "Smarttube";
 //const char *password = "GC2019";
 
 int count = 0;
+int IndentID= 0;
+int gcount = 0;
 
 
 AsyncWebServer server(80);
+
+int DropCounter(){
+    sleep(2000);
+    Serial.println("Internal count");
+    return 1;
+  }
+
  
 void setup(){
   Serial.begin(115200);
@@ -25,21 +36,79 @@ void setup(){
 
 
 
+
+ 
+server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request){
+    char send_data[16]="";
+    int paramsNr = request->params();
+    Serial.println(paramsNr);
+ 
+    for(int i=0;i<paramsNr;i++){
+         
+        AsyncWebParameter* p = request->getParam(i);
+        Serial.print("Param name: ");
+        Serial.println(p->name());
+        Serial.print("Param value: ");
+        Serial.println(p->value());
+        
+        if(p->name()=="MedID"){
+          //int Med = memory lookup (Offset,p->value())
+          Serial.println("Got Gcount from Memory");
+          // Static Value, change
+          itoa(5, send_data,10);
+          };
+          if(p->name()=="hMedID"){
+          //int gcount = memory lookup (Offset,p->value())
+          Serial.println("Got Gcount from Memory");
+          // Static Value, change
+          itoa(3, send_data,10);
+          };
+        if(p->name()=="EXP"){
+          //int gcount = memory lookup (Offset,p->value())
+          Serial.println("Got Gcount from Memory");
+          // Static Value, change
+          itoa(13, send_data,10);
+          };
+        if(p->name()=="UserID"){
+          //int gcount = memory lookup (Offset,p->value())
+          Serial.println("Got Gcount from Memory");
+          // Static Value, change
+          itoa(42, send_data,10);
+          };
+        if(p->name()=="GCount"){
+          //int gcount = memory lookup (Offset,p->value())
+          Serial.println("Got Gcount from Memory");
+          itoa(gcount, send_data,10);
+          };
+        if(p->name()=="count"){
+          //int gcount = memory lookup (Offset,p->value())
+          Serial.println("Got Gcount from Memory");
+          itoa(count, send_data,10);
+          };
+        if(p->name()=="reset"){
+          //int gcount = memory lookup (Offset,p->value())
+          Serial.println("Got Gcount from Memory");
+          count = 0;
+          };
+        Serial.println("------");
+    }
+    AsyncWebServerResponse *response = request->beginResponse_P(200, "text/plain", send_data);
+    response->addHeader("Server","Grab data");
+    request->send(response);
+    });
+
   server.on("/count", HTTP_GET, [](AsyncWebServerRequest *request){
     char cstr[16];
     itoa(count, cstr, 10);
-    AsyncWebServerResponse *response = request->beginResponse_P(200, "text/html", cstr);
+    AsyncWebServerResponse *response = request->beginResponse_P(200, "text/plain", cstr);
     response->addHeader("Server","Dropcount");
     int paramsNr = request->params();
     Serial.println(count);
- 
-    
     request->send(response);
   });
- 
 
   server.on("/reset", HTTP_GET, [](AsyncWebServerRequest *request){
-    AsyncWebServerResponse *response = request->beginResponse_P(200, "text/html", "success");
+    AsyncWebServerResponse *response = request->beginResponse_P(200, "text/plain", "success");
     response->addHeader("Server","Resetcount");
     int paramsNr = request->params();
     //Serial.println(paramsNr);
@@ -48,7 +117,7 @@ void setup(){
   });
 
    server.on("/inc", HTTP_GET, [](AsyncWebServerRequest *request){
-    AsyncWebServerResponse *response = request->beginResponse_P(200, "text/html", "incremented");
+    AsyncWebServerResponse *response = request->beginResponse_P(200, "text/plain", "incremented");
     response->addHeader("Server","Increment");
     int paramsNr = request->params();
     count = count +1;
@@ -57,8 +126,10 @@ void setup(){
   });
 
   server.on("/ident", HTTP_GET, [](AsyncWebServerRequest *request){
-    
-    AsyncWebServerResponse *response = request->beginResponse_P(200, "text/html", "Medication ID");
+    char idcstr[16];
+    // Static Value, change
+    itoa(5, idcstr, 10);
+    AsyncWebServerResponse *response = request->beginResponse_P(200, "text/plain", idcstr);
     response->addHeader("Server","MID");
     int paramsNr = request->params();
     Serial.println(count);
@@ -66,7 +137,7 @@ void setup(){
   });
 
   server.on("/exp", HTTP_GET, [](AsyncWebServerRequest *request){
-    AsyncWebServerResponse *response = request->beginResponse_P(200, "text/html", "Expiration Date");
+    AsyncWebServerResponse *response = request->beginResponse_P(200, "text/plain", "Expiration Date");
     response->addHeader("Server","Expiration");
     int paramsNr = request->params();
     count = count +1;
@@ -75,9 +146,9 @@ void setup(){
   });
  
   server.begin();
+  
 }
  
 void loop(){
-  
   }
 
